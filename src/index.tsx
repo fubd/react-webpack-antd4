f9fcdf5bd53083171a09169cbc6b7d697a94d462
fubd <312bdf@gmail.com>
2024-08-09 10:55:29 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useReducer, useState } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Button, Modal } from 'antd';
-import { wrapper } from './index.module.less';
-import { Provider, connect, useDispatch, useSelector } from 'react-redux';
+import React, {useContext, useEffect, useReducer, useState} from 'react';
+import {createRoot} from 'react-dom/client';
+import {Button, Modal} from 'antd';
+import {wrapper} from './index.module.less';
+import {Provider, connect, useDispatch, useSelector} from 'react-redux';
 import store from './store';
-import { createSelector } from 'reselect';
+import {createSelector} from 'reselect';
 
 function test(p: string) {
   return 'ok';
@@ -34,14 +34,13 @@ function App() {
 
   return (
     <div>
-      hey, <Button onClick={() => setState({ open: true })}>click open modal1</Button>
+      hey, <Button onClick={() => setState({open: true})}>click open modal1</Button>
       <br />
       <Button type="primary">click2</Button>
       <div className={wrapper}></div>
-      <Modal open={state.open} onCancel={() => setState({ open: false })}>
+      <Modal open={state.open} onCancel={() => setState({open: false})}>
         11213
       </Modal>
-
       <Button onClick={() => setCount(count + 1)}>click to increase,, {count}</Button>
     </div>
   );
@@ -50,31 +49,37 @@ function App() {
 const Home = (props) => {
   // console.log('props', props.userId)
 
-  const userId = useSelector(state => state.userId)
-  const dispatch = useDispatch()
+  const userId = useSelector((state: any) => state.userId);
+  const dispatch = useDispatch();
   return (
     <div>
       123, home,
       <hr></hr>
-      <button onClick={() => { dispatch({ type: 'ADD_ID', payload: userId + 1 }) }}>click, {userId}</button>
+      <button
+        onClick={() => {
+          dispatch({type: 'ADD_ID', payload: userId + 1});
+        }}
+      >
+        click, {userId}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => {
-  console.log('.....', state)
-  return { ...state }
-}
+  console.log('.....', state);
+  return {...state};
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     addId: (v) => {
-      dispatch({ type: 'ADD_ID', payload: v })
-    }
-  }
-}
+      dispatch({type: 'ADD_ID', payload: v});
+    },
+  };
+};
 
-const Home1 = connect(mapStateToProps, mapDispatchToProps)(Home)
+const Home1 = connect(mapStateToProps, mapDispatchToProps)(Home);
 
 const Lily = () => {
   return (
@@ -86,69 +91,94 @@ const Lily = () => {
         </div>
       </Provider>
     </div>
-  )
-}
+  );
+};
 
 const Rose = () => {
   const data = {
     name: 'jack',
-    age: 10
-  }
+    age: 10,
+  };
   function calc() {
-    console.log('>>>>>>')
+    console.log('>>>>>>');
     return data.name + data.age;
   }
 
-  const calc1 = createSelector(calc)
-
+  const calc1 = createSelector(calc);
 
   return (
     <div>
       <button onClick={calc}>click</button>
     </div>
-  )
-}
+  );
+};
+
+const MyContext = React.createContext<any>(null);
 
 function C3() {
-  const contextValue = useContext();
-  
-  return (
-    <div style={{ border: '1px solid powderblue' }}>
-      c3
-    </div>
-  )
+  // const contextValue = useContext(MyContext);
+  console.log('render c3...');
+  // const userId = useSelector((state: any) => state.userId);
+  return <div style={{border: '1px solid powderblue'}}>c3</div>;
 }
 
 function C1() {
   return (
-    <div style={{ border: '1px solid hotpink' }}>
+    <div style={{border: '1px solid hotpink'}}>
       c1
       <C3></C3>
     </div>
-  )
+  );
+}
+
+const C41 = connect((state: any) => {
+  return {userId: state.userId};
+})(C4);
+
+function C4(props: any) {
+  console.log('render c4...');
+  const userId = useSelector((state: any) => state.userId);
+  // const contextValue = useContext(MyContext);
+
+  return <div style={{border: '1px solid green'}}>c4, {userId}</div>;
 }
 
 function C2() {
+  console.log('c2')
   return (
-    <div>
-      c2
+    <div style={{border: '1px solid orange', margin: 24}}>
+      c2 <C4></C4>
     </div>
-  )
+  );
 }
 
-const MyContext = React.createContext<any>(null);
-function Daisy() {
-  const [count, setCount] = useState(0)
+function Daisy(props: any) {
+  const [count, setCount] = useState(0);
+  const dispatch = useDispatch();
+  console.log('~~');
   return (
     <div>
-      <button>click</button>
+      <button onClick={() => setCount(count + 1)}>click1</button>
+      <button onClick={() => dispatch({type: 'ADD_ID', payload: (Math.random() * 1000).toString().slice(0, 2)})}>
+        click
+      </button>
       <br />
       <MyContext.Provider value={count}>
         <C1></C1>
         <C2></C2>
       </MyContext.Provider>
     </div>
-  )
+  );
 }
 
-createRoot(document.getElementById('app') as HTMLElement).render(<Daisy />);
+const Daisy1 = connect()(Daisy);
+
+const App1 = () => {
+  return (
+    <Provider store={store}>
+      <Daisy1 />
+    </Provider>
+  );
+};
+
+createRoot(document.getElementById('app') as HTMLElement).render(<App1 />);
--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
+import {createStore, applyMiddleware} from 'redux';
 
-export default createStore((preState: any, action: { type: any, payload: any }) => {
-    if (action.type === 'ADD_ID') {
-        return {
-            ...preState,
-            userId: action.payload
-        }
-    }
+export default createStore((preState: any, action: {type: any; payload: any}) => {
+  if (action.type === 'ADD_ID') {
     return {
-        userId: 123
-    }
-})
+      ...preState,
+      userId: action.payload,
+    };
+  }
+  return {
+    userId: 123,
+  };
+});
